53fb713a78ed8dcaac7bf85667e98d43e7d4e2c6
건창 <thisisspear@geonchang-ui-MacBookAir.local>
2020-02-07 23:36:49 +0900

    스프링 부트 MVC 패턴으로 REST API 구현 1. 기본적인 패키지 생성

---
+++ data-rest/src/main/java/datarest/domain/enums/SocialType.java
@@ -0,0 +1,22 @@
+package datarest.domain.enums;
+
+public enum SocialType {
+    FACEBOOK("facebook"),
+    GOOGLE("google"),
+    KAKAO("kakao");
+
+    private final String ROLE_PREFIX = "ROLE_";
+    private String name;
+
+    SocialType(String name) {
+        this.name=name;
+    }
+
+    public String getRoleType(){return ROLE_PREFIX+name.toUpperCase();}
+
+    public  String getValue(){return name; }
+
+    public boolean isEaquals(String authority){
+        return this.getRoleType().equals(authority);
+    }
+}
